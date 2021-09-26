8a834cbe76500ee1ed32efde5b240963542314ad
WangWei <1204706553@qq.com>
2021-09-26 13:37:05 +0800

    没想到吧我又修改回来了

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("其他人修改了你的代码");
+        System.out.println("没想到吧我又修改回来了");
     }
 }
