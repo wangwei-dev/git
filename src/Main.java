5931a0d9571e8038e5f7bba10476ce3561ae6108
WangWei <1204706553@qq.com>
2021-09-26 14:33:04 +0800

    添加了新的代码

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,19 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("没想到吧我又修改回来了");
+        System.out.println("xxxx");
         System.out.println("yyyyy");
         System.out.println("2小组变动!");
         System.out.println("3小组变动!");
         System.out.println("4小组变动!");
+
+        //我添加了新的代码
+        if ("" == "") {
+            System.out.println("true");
+        }else {
+            System.out.println(false);
+        }
+
         System.out.println("5小组变动!");
 
         //添加了1行注释
