c5f82d38aa5dc000a4f440a422f2e96acd712198
王伟 <1204706553@qq.com>
2021-09-26 05:33:14 +0000

    update src/Main.java. 修改了myself

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("myself");
+        System.out.println("其他人修改了你的代码");
     }
 }
