b9e5ecb0791a15e0b31f30ba598deb4be9e05221
王伟 <1204706553@qq.com>
2021-09-26 06:30:29 +0000

    update src/Main.java.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("没想到吧我又修改回来了");
-        System.out.println("1小组变动!");
+        System.out.println("一小组变动!");
         System.out.println("2小组变动!");
         System.out.println("3小组变动!");
         System.out.println("4小组变动!");
