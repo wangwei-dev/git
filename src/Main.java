fcbf589fa4e9e71636594f40be5472cf856163bb
WangWei <1204706553@qq.com>
2021-09-26 17:18:41 +0800

    删除了map的1个put

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
         //我添加的新代码
         Map<String, String> map = new HashMap<>();
-        map.put("1", "11565652");
+        map.put("1", "11565652454545");
         System.out.println(map.get("1"));
 
         System.out.println("2小组变动!");
