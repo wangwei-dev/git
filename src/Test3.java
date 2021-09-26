2c72412e8e8826f925330869306ac057331d9f25
WangWei <1204706553@qq.com>
2021-09-26 14:21:20 +0800

    添加了一些类2

---
+++ src/Test3.java
@@ -0,0 +1,10 @@
+/**
+ * @Author WangWei
+ * @Date 2021/9/26 14:14
+ * @Version 1.0
+ */
+public class Test3 {
+    public static void main(String[] args) {
+        System.out.println("Test3");
+    }
+}
---
+++ src/Test4.java
@@ -0,0 +1,10 @@
+/**
+ * @Author WangWei
+ * @Date 2021/9/26 14:15
+ * @Version 1.0
+ */
+public class Test4 {
+    public static void main(String[] args) {
+        System.out.println("Test4");
+    }
+}
